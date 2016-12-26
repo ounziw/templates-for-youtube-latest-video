5ead105a392d2f075dab6a0b59efcc6db76586e2
ounziw <ounziw@gmail.com>
2016-12-26 13:44:38 +0900

    first commit

---
+++ application/blocks/youtube_latest_video_playlist/templates/list3col3row/view.php
@@ -0,0 +1,11 @@
+<?php
+defined('C5_EXECUTE') or die("Access Denied.");
+$number_of_videos = 9;
+$video_css = 'col-xs-4 small-4';
+?>
+
+<?php
+$this->inc('listtemplate.php', array(
+    'number_of_videos' => $number_of_videos,
+    'video_css' => $video_css,
+)); ?> 
