e36a4060b20c87e43d32d26d233b68ea48d687e3
ounziw <ounziw@gmail.com>
2016-12-26 13:46:48 +0900

    remove unnnecessary php tag

--- application/blocks/youtube_latest_video_playlist/templates/list2col3row/view.php
+++ application/blocks/youtube_latest_video_playlist/templates/list2col3row/view.php
@@ -2,10 +2,8 @@
 defined('C5_EXECUTE') or die("Access Denied.");
 $number_of_videos = 6;
 $video_css = 'col-xs-6 small-6';
-?>
 
-<?php
 $this->inc('listtemplate.php', array(
     'number_of_videos' => $number_of_videos,
     'video_css' => $video_css,
-)); ?> 
+));
--- application/blocks/youtube_latest_video_playlist/templates/list4col2row/view.php
+++ application/blocks/youtube_latest_video_playlist/templates/list4col2row/view.php
@@ -2,10 +2,8 @@
 defined('C5_EXECUTE') or die("Access Denied.");
 $number_of_videos = 8;
 $video_css = 'col-xs-6 small-6 col-sm-3 large-3';
-?>
 
-<?php
 $this->inc('listtemplate.php', array(
     'number_of_videos' => $number_of_videos,
     'video_css' => $video_css,
-)); ?> 
+));
